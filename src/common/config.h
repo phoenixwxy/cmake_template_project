88d8c80daf0fae19df2dbd00217cff78500f0a8e
phoenixwxy <wxymails@qq.com>
2020-12-17 15:12:24 +0800

    remove Log and Utils version
    Signed-off-by: phoenixwxy <wxymails@qq.com>

--- src/common/config.h
+++ src/common/config.h
@@ -1,12 +1,8 @@
+#ifndef __PROJECT_CONFIG_H
+#define __PROJECT_CONFIG_H
 // the configured optins and setting for version
 #define PROJECT_VERSION_MAJOR 0
 #define PROJECT_VERSION_MINOR 1
 #define PROJECT_VERSION_PATCH 0
 
-#define PLOG_VERSION_MAJOR 0
-#define PLOG_VERSION_MINOR 1
-#define PLOG_VERSION_PATCH 0
-
-#define UTILS_VERSION_MAJOR 0
-#define UTILS_VERSION_MINOR 1
-#define UTILS_VERSION_PATCH 0
+#endif
